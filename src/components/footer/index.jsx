ff7953c97de74fee1434f7f940c666e712afa79c
marco-soria <marco.soriaa7@gmail.com>
2025-10-13 08:46:29 -0500

    updated images and footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,74 +1,83 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { FaFacebook, FaInstagram, FaTiktok, FaYoutube } from 'react-icons/fa';
-import { FaXTwitter } from 'react-icons/fa6';
-import { IconContext } from 'react-icons';
-import { Link } from 'react-router-dom'
-import './Footer.css'; // Importa el archivo de estilos local
-import { FaHeart } from "react-icons/fa";
-import { FaCartShopping } from "react-icons/fa6";
-import { isAuthenticated } from '../../helpers/auth';
-import { AuthContext } from '../../pages/auth/AuthContext';
+import { useContext } from "react";
+import { IconContext } from "react-icons";
+import {
+  FaFacebook,
+  FaHeart,
+  FaInstagram,
+  FaTiktok,
+  FaYoutube,
+} from "react-icons/fa";
+import { FaCartShopping, FaXTwitter } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../pages/auth/AuthContext";
+import "./Footer.css"; // Importa el archivo de estilos local
 
 const Footer = () => {
   const { isUserAuthenticated } = useContext(AuthContext);
-  
-
-  
-
-  
-  
 
   return (
     <footer>
       <div className="footerContainer">
         <div className="mediaIcons">
           <IconContext.Provider value={{ classname: "footericons" }}>
-          <Link to="/"><FaFacebook className='fa-icon'/></Link>
-          <Link to="/"><FaInstagram className='fa-icon' /></Link>
-          <Link to="/"><FaXTwitter  className='fa-icon'/></Link>
-          <Link to="/"><FaTiktok className='fa-icon' /></Link>
-          <Link to="/"><FaYoutube className='fa-icon'/></Link>
+            <Link to="/">
+              <FaFacebook className="fa-icon" />
+            </Link>
+            <Link to="/">
+              <FaInstagram className="fa-icon" />
+            </Link>
+            <Link to="/">
+              <FaXTwitter className="fa-icon" />
+            </Link>
+            <Link to="/">
+              <FaTiktok className="fa-icon" />
+            </Link>
+            <Link to="/">
+              <FaYoutube className="fa-icon" />
+            </Link>
           </IconContext.Provider>
         </div>
         <div className="footerNav">
-        <ul className= "footer-nav">
-        <li>
-          <Link to="/" className="nav-links" >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/anime" className="nav-links" >
-            Anime
-          </Link>
-        </li>
-        <li>
-          <Link to="/news" className="nav-links" >
-            News
-          </Link>
-        </li>
-        <li>
-          <Link to="/store" className="nav-links" >
-            Store
-          </Link>
-        </li>
-        {isUserAuthenticated && (
-          <li>
-            <Link to="/wishlist" className="nav-links" >
-              <FaHeart></FaHeart>
-            </Link>
-          </li>
-        )}
-        <li>
-          <Link to="/cart" className="nav-links" >
-            <FaCartShopping></FaCartShopping>
-          </Link>
-        </li>
-        
-      </ul>
+          <ul className="footer-nav">
+            <li>
+              <Link to="/" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/anime" className="nav-links">
+                Anime
+              </Link>
+            </li>
+            <li>
+              <Link to="/news" className="nav-links">
+                News
+              </Link>
+            </li>
+            <li>
+              <Link to="/store" className="nav-links">
+                Store
+              </Link>
+            </li>
+            {isUserAuthenticated && (
+              <li>
+                <Link to="/wishlist" className="nav-links">
+                  <FaHeart></FaHeart>
+                </Link>
+              </li>
+            )}
+            <li>
+              <Link to="/cart" className="nav-links">
+                <FaCartShopping></FaCartShopping>
+              </Link>
+            </li>
+          </ul>
         </div>
         <div className="footerCopyright">
-          <p>Copyright &copy;2024; Designed by <span className="designer">Marco Antonio</span></p>
+          <p>
+            Copyright &copy;{new Date().getFullYear()}; Designed by{" "}
+            <span className="designer">Marco Antonio</span>
+          </p>
         </div>
       </div>
     </footer>
